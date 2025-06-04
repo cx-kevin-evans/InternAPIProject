d99c8b8faefd5c1959bbf53d78d8875f17924177
Isha Jannu <212002468+cx-isha-jannu@users.noreply.github.com>
2025-06-04 16:10:34 -0500

    final

--- AuditScratch.py
+++ AuditScratch.py
@@ -1,6 +1,7 @@
 import requests
 import argparse
-    # Parse and handle various CLI flags
+
+# Obtain command line arguments
 parser = argparse.ArgumentParser(description='Export a CxOne scan workflow as a CSV file')
 parser.add_argument('--region', required=True, help='Region for the API endpoint (e.g., us, eu)')
 parser.add_argument('--tenant_name', required=True, help='Tenant name')
@@ -11,12 +12,6 @@ args = parser.parse_args()
 region = args.region
 tenantName = args.tenant_name
 apiKey = args.api_key
- 
-# Custom script variables
-region = "us"
-tenantName = "evanskevin-sedemo"
-apiKey = "eyJhbGciOiJIUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI5NjI2Zjc1Zi1kZTAwLTQwOTQtYjNjNC0wZjk3ZWZkOTA2YzUifQ.eyJpYXQiOjE3NDg0NjMzMDgsImp0aSI6IjhiZWZmYzkwLTQzMmMtNGQwMC1hNTM3LTA0NDhhY2Y0YmYxMyIsImlzcyI6Imh0dHBzOi8vdXMuaWFtLmNoZWNrbWFyeC5uZXQvYXV0aC9yZWFsbXMvZXZhbnNrZXZpbi1zZWRlbW8iLCJhdWQiOiJodHRwczovL3VzLmlhbS5jaGVja21hcngubmV0L2F1dGgvcmVhbG1zL2V2YW5za2V2aW4tc2VkZW1vIiwic3ViIjoiNDFjZmFhMzEtMjVkNC00NGFjLThkNDMtMTk3MzQxZGY5MzBhIiwidHlwIjoiT2ZmbGluZSIsImF6cCI6ImFzdC1hcHAiLCJzaWQiOiIyYTQ3NjgwMi0wODRmLTQ3MGUtYjAzNS04ZjMwZjMzMzUyYTMiLCJzY29wZSI6ImFzdC1hcGkgZW1haWwgcHJvZmlsZSBncm91cHMgaWFtLWFwaSByb2xlcyBvZmZsaW5lX2FjY2VzcyJ9.DQphOxHwY0epHyKPyOymzzBP_mBf50vvyNT5zipAfSOoUHXPHb0O_7DhgcbZPNT-70oSqM0kPIrGXsmV_pszww"
-
 
 # # Generating a new access token via the API key
 url = "https://" + region + ".iam.checkmarx.net/auth/realms/" + tenantName + "/protocol/openid-connect/token"
