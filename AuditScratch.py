d497be46fee990c00dfe1dd5375d216a78de5c9f
Isha Jannu <212002468+cx-isha-jannu@users.noreply.github.com>
2025-06-06 09:42:49 -0500

    dates

--- AuditScratch.py
+++ AuditScratch.py
@@ -39,7 +39,7 @@ headers = {
 # Make the GET request
 try:
     response = requests.get(audit_url, headers=headers)
-    print("Audit API response:", response.json()["eventDate"])
+    print("Audit API response:", response.json()["events"])
     
     # Check for status 200
     if response.status_code == 200:
