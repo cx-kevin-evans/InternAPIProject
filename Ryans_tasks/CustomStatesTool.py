f1ffa35af7cdc2f69a31231654bbc8588ad88583
Isha Jannu <212002468+cx-isha-jannu@users.noreply.github.com>
2025-07-02 13:28:13 -0500

    debug

--- Ryans_tasks/CustomStatesTool.py
+++ Ryans_tasks/CustomStatesTool.py
@@ -127,9 +127,8 @@ def get_state_list():
                 print(state)
         else:
             print(f"Response status code: {response.status_code}")
-            if debug:
-                print("url: " + url)
-                print("response text: " + response.text)
+            print("url: " + url)
+            print("response text: " + response.text)
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while fetching custom states: {e}")
